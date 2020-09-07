15dcc01167b7db250377552c8c7684a72b5b5cfe
escarokokolollol <oscar_ek21@hotmail.com>
2020-09-07 15:59:41 +0200

    new paragraph

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>pwa test</p>
+        <p>new paragraph </p>
         <a
           className="App-link"
           href="https://reactjs.org"
