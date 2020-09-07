ceed8a78d885f529ac5f6453f901285c5dace0a4
escarokokolollol <oscar_ek21@hotmail.com>
2020-09-07 16:06:10 +0200

    Updates

---
+++ precache-manifest.0029fcc295c729cf1dbdf185cd64a3df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e04eb2227a8469be000b0cfc833f5b",
+    "url": "/pwa-test/index.html"
+  },
+  {
+    "revision": "5f6edc14ecdb586be701",
+    "url": "/pwa-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "260c5df875fd93a80636",
+    "url": "/pwa-test/static/js/2.3084209f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa-test/static/js/2.3084209f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6edc14ecdb586be701",
+    "url": "/pwa-test/static/js/main.6ccefc78.chunk.js"
+  },
+  {
+    "revision": "92535314b103e3167ccd",
+    "url": "/pwa-test/static/js/runtime-main.3cb95e32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
